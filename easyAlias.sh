09011a10d2de545d65e3bc6c248c442bde91c2f5
cs-499243 <63954929+cs-499243@users.noreply.github.com>
2020-12-05 19:04:34 +0000

    Create easyAlias.sh

---
+++ easyAlias.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add to .bashrc or .bash_profile: 
+#	easyLoc="{location of easyAlias.sh}"; source "$easyLoc/easyAlias.sh"
+
+alias ad="$easyLoc/listerFix.sh -m manual"
+alias AD="$easyLoc/listerFix.sh"
