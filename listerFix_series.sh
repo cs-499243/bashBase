780275a4b97ad62d1f031f5d474be39c6db99a31
cs-499243 <co.sa@tuta.io>
2020-12-07 18:34:29 +0000

    minor changes to comments and removing spaces

--- listerFix_series.sh
+++ listerFix_series.sh
@@ -3,7 +3,7 @@
 # $1=TITLE* $2=NUM* $3=FILLER $4=URL
 
 function badParameters { 
-	echo " - ERROR - $1 ${2:+or $2}"
+	echo " - ERROR - $1${2:+ or $2}"
 	echo "Correct parameters are ([TITLE]* [NUM]* [FILLER] [URL]) where * is required"
 	exit 1
 	}
@@ -17,6 +17,8 @@ bash $bashBase/listerFix.sh -i $2 ${3:+-f $3} ""
 
 # If URL is given, make link file
 [[ -n $4 ]] && echo -e "[Desktop Entry]\nIcon=text-html\nType=Link\nURL[\$e]=$4" > _LINK
+
+# Return - not essential but clean
 cd -
 
 # TODO
