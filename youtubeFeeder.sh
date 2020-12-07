8f77872f5ccb8973cf769a15163d02f4d765a100
cs-499243 <co.sa@tuta.io>
2020-12-07 19:44:51 +0000

    Fix youtubeFeeder so it can make links to youtube items

--- youtubeFeeder.sh
+++ youtubeFeeder.sh
@@ -1,5 +1,25 @@
-echo $1
+# https://superuser.com/questions/1341684/youtube-dl-how-download-only-the-playlist-not-the-files-therein
 
+cleanList=$(youtube-dl -j --flat-playlist $1 | cut -c109- | rev | cut -c43- | rev)
+
+OLDIFS=$IFS; IFS=$'\n'
+
+cleanTitles=($(echo "$cleanList" | cut -c11- | rev | cut -c24- | rev))
+cleanURLS=($(echo "$cleanList" | rev | cut -c2-12 | rev))
+
+IFS=$OLDIFS
+
+for ((i=0 ; i<${#cleanTitles[@]} ; i++)); do
+	itemTitle="${cleanTitles[i]}"
+	itemURL="${cleanURLS[i]}"
+	itemURLfull="https://www.youtube.com/watch?v=$itemURL"
+	
+	echo -e "$itemTitle - $itemURLfull"
+	
+	echo -e "[Desktop Entry]\nIcon=text-html\nType=Link\nURL[\$e]=$itemURLfull" > "$itemTitle"
+done
+
+: '
 case $1 in
 	*.zip)
 		# $1 = NAME, $2 = VIDEO
@@ -21,3 +41,9 @@ case $1 in
 		rm -r $newName
 		exit 0;;
 esac
+'
+
+# TODO
+# - Get rid of the old code (replace it with something better)
+# - Arrange the new data in a useable form
+# - Make sure it allows for higher length videos (will mess up the cutting of cleanList)
