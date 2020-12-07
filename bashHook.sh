2b4383fb6b6cdedea097fa67d56a8f8600e21a92
cs-499243 <co.sa@tuta.io>
2020-12-07 18:31:40 +0000

    Reorganise files and fix up listerFix so it works cleaner

--- bashHook.sh
+++ bashHook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Add to .bashrc or .bash_profile: 
-#	export bashBase="LOCATION OF THIS FOLDER"; source $bashBase/"THIS FILE"
+#	export bashBase="LOCATION OF THIS FOLDER"; source $bashBase/bashHook.sh
 
 alias ad="$bashBase/listerFix.sh -m manual"
 alias ad_auto="$bashBase/listerFix.sh"
-alias ad_series="$bashBase/seriesSet.sh"
+alias ad_series="$bashBase/listerFix_series.sh"
---
+++ listerFix_series.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# $1=TITLE* $2=NUM* $3=FILLER $4=URL
+
+function badParameters { 
+	echo " - ERROR - $1 ${2:+or $2}"
+	echo "Correct parameters are ([TITLE]* [NUM]* [FILLER] [URL]) where * is required"
+	exit 1
+	}
+
+[[ -d "$1" || -z $1 ]]	&& badParameters "TITLE" "folder exists"
+[[ -z $2 ]]				&& badParameters "NUM"
+
+# Make folder with series expanded inside - if filler is given, used
+mkdir "$1"; cd "$1"
+bash $bashBase/listerFix.sh -i $2 ${3:+-f $3} ""
+
+# If URL is given, make link file
+[[ -n $4 ]] && echo -e "[Desktop Entry]\nIcon=text-html\nType=Link\nURL[\$e]=$4" > _LINK
+cd -
+
+# TODO
+# Add in a loop to allow for all parameters in standard way
+# Allow for a forced override of exit conditions
