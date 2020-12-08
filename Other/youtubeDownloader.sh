8f554c796cec9b3b63e3d22b8aeafd481c7718c9
cs-499243 <co.sa@tuta.io>
2020-12-08 00:22:07 +0000

    Remove old youtube interface

--- Other/youtubeDownloader.sh
+++
@@ -1,6 +0,0 @@
-if [ "$(grep -s -A 1 $1 ~/.newsboat/YoutubeTitle.txt | tail -1)" == "" ]; then
-	echo $1 >> ~/.newsboat/YoutubeTitle.txt
-	youtube-dl --get-filename -o '~/Videos/%(title)s' -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' $1 >> ~/.newsboat/YoutubeTitle.txt
-fi
-
-youtube-dl -q -o '~/Videos/%(title)s' -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' $1 &
--- Other/youtubeRemover.sh
+++
@@ -1,6 +0,0 @@
-vidTitle="$(grep -A 1 $1 ~/.newsboat/YoutubeTitle.txt | tail -1)"
-
-#vidExist=$(ls "$vidTitle"*) && rm "$vidExist"
-
-#grep -v "$vidTitle" ~/.newsboat/YoutubeTitle.txt #> ~/.newsboat/YoutubeTitle.txt
-#grep -v "$1" ~/.newsboat/YoutubeTitle.txt > ~/.newsboat/YoutubeTitle.txt
