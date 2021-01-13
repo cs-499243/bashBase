79166ecd2cb2ddfc57fb9bb017c586942df27949
cs-499243 <co.sa@tuta.io>
2021-01-13 14:31:05 +0000

    Restructure bashBase

--- bashHook.sh
+++ bashHook.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Add to .bashrc or .bash_profile: 
-#	export bashBase="LOCATION OF THIS FOLDER"; source $bashBase/bashHook.sh
+#	export bashBase="LOCATION OF THIS FOLDER"; source "$bashBase/bashHook.sh"
 
 # Function to allow you to do (e.g.) "bashRelay listerFix.sh" instead of writing "bash $bashBase/listerFix.sh"
 function bashRelay {
 	# If a program name is given, try to open it. If not, list programs
-	if [[ -n $1 ]]; then bash $bashBase/$1; else ls $bashBase; fi
+	if [[ -n $1 ]]; then bash $bashBase/$1; else ls $(ls -d */); fi
 	}
 
-# Export ~ as a specific variable - allows you to use ~ in ls and things like it
-export baseLoc=~
 # Give aliases to my most used programs
-alias ad="$bashBase/listerFix.sh -m manual"
-alias ad_auto="$bashBase/listerFix.sh -m automatic"
-alias ad_series="$bashBase/listerFix_series.sh"
+alias ad="$bashBase/listerFix/listerFix.sh -m manual"
+alias ad_auto="$bashBase/listerFix/listerFix.sh"
+alias ad_series="$bashBase/listerFix/listerFix_series.sh"
+alias refresh="source $HOME/.bash_profile"
+
+alias bashHook="echo 'Methods: bashRelay, ad, ad_auto, ad_series, refresh'"
